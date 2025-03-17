cbb4a4b64264d2977bd4c7c0a86cc7ae72c73a6b
AMuhammadFarhanHakzah <amf21022002@gmail.com>
2025-03-17 08:39:35 +0800

    signIn DONE

--- src/components/AuthModel.tsx
+++ src/components/AuthModel.tsx
@@ -5,6 +5,8 @@ import { Dialog, DialogContent } from './ui/dialog'
 import { Input } from './ui/input'
 import { createClient } from '@/utils/supabase/client'
 import { Button } from './ui/button'
+import { toast } from 'sonner'
+import { DialogTitle } from '@radix-ui/react-dialog'
 
 const AuthModel = () => {
 
@@ -13,15 +15,20 @@ const AuthModel = () => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
 
+    const [isLoading, setIsLoading] = useState(false)
+
 
     useEffect(() => {
         const checkAuth = async () => {
             const supabase = createClient()
             const { data: { user }, error } = await supabase.auth.getUser()
-            console.log(user, error);
+            console.log("USER: ", user);
+            console.log("ERROR: ", error);
 
             if (!user) {
                 setIsOpen(true)
+            } else {
+                setIsOpen(false)
             }
         }
 
@@ -31,6 +38,7 @@ const AuthModel = () => {
     return (
         <div>
             <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                <DialogTitle></DialogTitle>
                 <DialogContent className='bg-black min-w-screen h-screen border border-transparent flex justify-center items-center' onInteractOutside={(e) => e.preventDefault()}>
                     <div className='w-2xl mr-30 '>
                         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5Z6h2su_P2Dpy48AmTVcigVGKB5bsYuMZQ&s' className='w-xs mx-auto' />
@@ -56,24 +64,46 @@ const AuthModel = () => {
 
             {/* SIGN UP DIALOG */}
             <Dialog open={signUp} onOpenChange={setSignUp}>
+                <DialogTitle></DialogTitle>
                 <DialogContent className='bg-black px-18' onInteractOutside={(e) => e.preventDefault()}>
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5Z6h2su_P2Dpy48AmTVcigVGKB5bsYuMZQ&s' className='w-8 mx-auto'/>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5Z6h2su_P2Dpy48AmTVcigVGKB5bsYuMZQ&s' className='w-8 mx-auto' />
                     <div className='font-bold text-3xl'>Create your account</div>
-                    <form onSubmit={(e) => {
+                    <form onSubmit={async (e) => {
                         e.preventDefault();
-                        supabase.auth.signInWithOtp({
+                        setIsLoading(true)
+
+                        const supabase = createClient()
+
+                        // Check if username exists
+                        const { data, error } = await supabase.from('profiles').select().eq('username', username.trim());
+
+                        if (error) {
+                            return toast.error("Failed to check username, please try again")
+                        }
+
+                        if (data && data.length > 0) {
+                            console.log(data)
+                            return toast.error("Username already existed")
+                        }
+
+                        localStorage.setItem("username", username.trim())
+                        localStorage.setItem("email", email.trim())
+
+                        await supabase.auth.signInWithOtp({
                             email: email.trim(),
                             options: {
-                                data:{
-                                    username
+                                data: {
+                                    username,
                                 }
                             }
 
                         })
+                        setIsLoading(false)
+
                     }}>
                         <Input type='text' onChange={(e) => setUsername(e.target.value)} min={3} name='name' placeholder="Name" className='my-4 py-7 placeholder:text-[17px] border border-white/40' />
                         <Input type='email' onChange={(e) => setEmail(e.target.value)} name='email' placeholder="Email" className='my-4 py-7 placeholder:text-[17px] border border-white/40' />
-                        <Button className='w-full rounded-full bg-white font-bold py-6 text-black hover:bg-white/90'>Next</Button>
+                        <Button className='w-full rounded-full bg-white font-bold py-6 text-black hover:bg-white/90' disabled={isLoading}>Next</Button>
                     </form>
                 </DialogContent>
             </Dialog>
