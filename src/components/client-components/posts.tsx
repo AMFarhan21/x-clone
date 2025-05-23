a773e35d513066d5b6d94fee5be99e8d1398c99d
AMFarhan21 <AMFarhan2102@gmail.com>
2025-05-23 17:42:48 +0800

    feat: show image

--- src/components/client-components/posts.tsx
+++ src/components/client-components/posts.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { BsDot } from "react-icons/bs";
 import { IoStatsChart } from "react-icons/io5";
@@ -12,119 +12,261 @@ import Repost from "./rePost-button";
 import BookmarkButton from "./bookmark-button";
 import Image from "next/image";
 import { Post, Profiles } from "@/types";
+import { useEffect, useState } from "react";
+import { XIcon } from "lucide-react";
 
 type postProps = {
-    post: Post;
-    userId?: string;
-    userProfiles: Profiles;
-}
+  post: Post;
+  userId?: string;
+  userProfiles: Profiles;
+};
 
 const Posts = ({ post, userId, userProfiles }: postProps) => {
-
-
-    const router = useRouter();
-
-    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.stopPropagation();
+  const [activeImage, setActiveImage] = useState<string | null>(null);
+  const handleActiveImage = (e: React.MouseEvent, fileUrl: string) => {
+    e.stopPropagation();
+    setActiveImage(fileUrl);
+  };
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    if (activeImage) {
+      const timeout = setTimeout(() => setShowModal(true), 10);
+      return () => clearTimeout(timeout);
+    } else {
+      setShowModal(false);
     }
+  }, [activeImage]);
 
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+  };
 
-
-    const handlePostClick = () => {
-        const select = window.getSelection();
-        if (select && select.toString().length > 0) {
-            return;
-        }
-        router.push(`/${post.username}/status/${post.id}`)
-        // const imageArray = JSON.parse(post.imageUrl);
+  const router = useRouter();
+  const handlePostClick = () => {
+    const select = window.getSelection();
+    if (select && select.toString().length > 0) {
+      return;
     }
+    router.push(`/${post.username}/status/${post.id}`);
+    // const imageArray = JSON.parse(post.imageUrl);
+  };
 
+  const handlePostProfileClick = () => {
+    window.location.href = `/${post.username}`;
+  };
 
-    const handlePostProfileClick = () => {
-        window.location.href = `/${post.username}`
-    }
+  const imageArray = post.imageUrl ? JSON.parse(post.imageUrl) : [];
 
+  return (
+    <div
+      key={post.id}
+      onClick={handlePostClick}
+      className="border-b border-gray-600/50 pt-3 w-full cursor-pointer hover:bg-white/2"
+    >
+      <div className="flex !px-2 sm:!px-4">
+        {post.profilePicture ? (
+          <Image
+            alt="profilePicture"
+            src={post.profilePicture}
+            width={300}
+            height={300}
+            loading="eager"
+            className="bg-white/50 w-10 h-10 rounded-full object-cover"
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePostProfileClick();
+            }}
+          />
+        ) : (
+          <div
+            className="bg-white/50 min-w-10 h-10 rounded-full"
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePostProfileClick();
+            }}
+          >
+            {" "}
+          </div>
+        )}
 
-    const imageArray = post.imageUrl ? JSON.parse(post.imageUrl) : [];
+        <div className="ml-2 sm:!ml-4 sm:w-full w-[85%]">
+          <div className="flex justify-between ">
+            <div className="flex flex-wrap min-w-0 items-center">
+              <div
+                className="font-semibold hover:border-b-2 border-white whitespace-nowrap "
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handlePostProfileClick();
+                }}
+              >
+                {post.displayName ? post.displayName : post.username}
+              </div>
 
-
-
-    return (
-        <div key={post.id} onClick={handlePostClick} className="border-b border-gray-600/50 pt-3 w-full cursor-pointer hover:bg-white/2">
-
-            <div className="flex !px-2 sm:!px-4">
-                {post.profilePicture ? (
-                    <Image alt="profilePicture" src={post.profilePicture} width={300} height={300} loading="eager" className="bg-white/50 w-10 h-10 rounded-full object-cover" onClick={(e) => { e.stopPropagation(); handlePostProfileClick() }} />
-                ) : (
-                    <div className="bg-white/50 min-w-10 h-10 rounded-full" onClick={(e) => { e.stopPropagation(); handlePostProfileClick() }}>  </div>
-                )}
-
-                <div className="ml-2 sm:!ml-4 sm:w-full w-[85%]">
-                    <div className="flex justify-between ">
-                        <div className="flex flex-wrap min-w-0 items-center">
-
-                            <div className="font-semibold hover:border-b-2 border-white whitespace-nowrap " onClick={(e) => { e.stopPropagation(); handlePostProfileClick() }}>{post.displayName ? post.displayName : post.username}</div>
-
-                            <div className="text-white/50 text-sm ml-1 whitespace-nowrap text-ellipsis overflow-hidden" onClick={(e) => { e.stopPropagation(); handlePostProfileClick() }}>@{post.username}</div>
-                            <div className="text-white/50 text-sm"> <BsDot /> </div>
-                            <div className="text-white/50 text-sm whitespace-nowrap overflow-x-hidden"> <DayJs date={post.created_at} profilesCreated={null} /> </div>
-
-                        </div>
-                        <div className="flex flex-wrap space-x-2 items-center">
-                            <div>Grok</div>
-                            <div>
-                                <DropdownButton<Post> username={post.username} data={post} userId={userId} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="pb leading-4.5 text-[15px] mt-2 overflow-x-scroll overflow-clip"> {post.text} </div>
-
-                    {
-                        imageArray.length > 0 && (<div className={`grid gap-[2px] mt-4 rounded-2xl w-[100%] overflow-hidden ${imageArray.length === 1 ? "grid-cols-1" : "grid-cols-2"} ${imageArray.length === 2 && "grid-cols-2 h-75 w-full"} ${imageArray.length === 3 && "grid-cols-2 grid-rows-2"} `}>
-                            {imageArray.slice(0, 4).map((fileUrl: string, index: number) => {
-                                const isVideo = fileUrl.endsWith(".mp4") || fileUrl.endsWith(".mov")
-
-                                return (
-                                    isVideo ? (
-                                        <video key={index} controls autoPlay loop playsInline className="w-full h-full object-cover">
-                                            <source src={fileUrl} type="video/mp4" />
-                                        </video>
-                                    ) : (
-                                        <Image src={fileUrl}
-                                            alt="Post Image"
-                                            width={500} height={500} loading="eager"
-                                            // sizes="10vw"
-                                            key={index}
-                                            className={`w-full h-full object-cover ${imageArray.length === 3 && index === 0 && "row-span-2"}`} />
-                                    )
-                                )
-                            })}
-                        </div>)
-                    }
-
-
-                    <div className="flex justify-between items-center">
-                        <div className="flex items-center text-white/50 text-[18px] " onClick={(e) => { e.stopPropagation() }}>
-                            <ReplyButton userProfiles={userProfiles} post={post} userId={userId} dataId={post.id} postUsername={post.username} replyCount={post.replyCount} />
-                        </div>
-                        <div className="flex items-center text-white/50 text-[18px]">
-                            <Repost postId={post.id} userId={userId} isRePosted={post.isRePosted} rePostCount={post.rePostCount} replyId={null} />
-                        </div>
-                        <div className="flex items-center text-white/50 text-[18px]">
-                            <LikeButton replyId={null} postId={post.id} likesCount={post.likesCount} isLiked={post.isLiked} userId={userId} />
-                        </div >
-                        <div className="flex items-center text-white/50 text-[18px]" >
-                            <button onClick={handleClick} className="flex rounded-full bg-transparent hover:bg-blue-400/10 hover:text-blue-400 p-2 my-1 text-white/35 items-end cursor-pointer space-x-1"> <IoStatsChart /><div className="mt-[3px] text-xs">1</div></button>
-                        </div>
-                        <div className="flex text-white/50 items-center space-x-0 text-[18px]">
-                            <BookmarkButton postId={post.id} userId={userId as string} replyId={null} isBookmarked={post.isBookmarked} />
-                            <button className="rounded-full bg-transparent hover:bg-blue-400/10 hover:text-blue-400 p-2 my-1 text-white/50 cursor-pointer" onClick={handleClick}> <FiShare /> </button>
-                        </div>
-                    </div>
-                </div>
+              <div
+                className="text-white/50 text-sm ml-1 whitespace-nowrap text-ellipsis overflow-hidden"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handlePostProfileClick();
+                }}
+              >
+                @{post.username}
+              </div>
+              <div className="text-white/50 text-sm">
+                {" "}
+                <BsDot />{" "}
+              </div>
+              <div className="text-white/50 text-sm whitespace-nowrap overflow-x-hidden">
+                {" "}
+                <DayJs date={post.created_at} profilesCreated={null} />{" "}
+              </div>
             </div>
-        </div>
-    )
-}
+            <div className="flex flex-wrap space-x-2 items-center">
+              <div>Grok</div>
+              <div>
+                <DropdownButton<Post>
+                  username={post.username}
+                  data={post}
+                  userId={userId}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="pb leading-4.5 text-[15px] mt-2 overflow-x-scroll overflow-clip">
+            {" "}
+            {post.text}{" "}
+          </div>
 
-export default Posts
+          {imageArray.length > 0 && (
+            <div
+              className={`grid gap-[2px] mt-4 rounded-2xl w-[100%] overflow-hidden ${
+                imageArray.length === 1 ? "grid-cols-1" : "grid-cols-2"
+              } ${imageArray.length === 2 && "grid-cols-2 h-75 w-full"} ${
+                imageArray.length === 3 && "grid-cols-2 grid-rows-2"
+              } `}
+            >
+              {imageArray.slice(0, 4).map((fileUrl: string, index: number) => {
+                const isVideo =
+                  fileUrl.endsWith(".mp4") || fileUrl.endsWith(".mov");
+
+                return isVideo ? (
+                  <video
+                    key={index}
+                    controls
+                    autoPlay
+                    loop
+                    playsInline
+                    className="w-full h-full object-cover"
+                  >
+                    <source src={fileUrl} type="video/mp4" />
+                  </video>
+                ) : (
+                  <Image
+                    src={fileUrl}
+                    alt="Post Image"
+                    width={500}
+                    height={500}
+                    loading="eager"
+                    // sizes="10vw"
+                    key={index}
+                    className={`w-full h-full object-cover ${
+                      imageArray.length === 3 && index === 0 && "row-span-2"
+                    }`}
+                    onClick={(e) => {
+                      handleActiveImage(e, fileUrl);
+                    }}
+                  />
+                );
+              })}
+            </div>
+          )}
+
+          <div className="flex justify-between items-center">
+            <div
+              className="flex items-center text-white/50 text-[18px] "
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <ReplyButton
+                userProfiles={userProfiles}
+                post={post}
+                userId={userId}
+                dataId={post.id}
+                postUsername={post.username}
+                replyCount={post.replyCount}
+              />
+            </div>
+            <div className="flex items-center text-white/50 text-[18px]">
+              <Repost
+                postId={post.id}
+                userId={userId}
+                isRePosted={post.isRePosted}
+                rePostCount={post.rePostCount}
+                replyId={null}
+              />
+            </div>
+            <div className="flex items-center text-white/50 text-[18px]">
+              <LikeButton
+                replyId={null}
+                postId={post.id}
+                likesCount={post.likesCount}
+                isLiked={post.isLiked}
+                userId={userId}
+              />
+            </div>
+            <div className="flex items-center text-white/50 text-[18px]">
+              <button
+                onClick={handleClick}
+                className="flex rounded-full bg-transparent hover:bg-blue-400/10 hover:text-blue-400 p-2 my-1 text-white/35 items-end cursor-pointer space-x-1"
+              >
+                {" "}
+                <IoStatsChart />
+                <div className="mt-[3px] text-xs">1</div>
+              </button>
+            </div>
+            <div className="flex text-white/50 items-center space-x-0 text-[18px]">
+              <BookmarkButton
+                postId={post.id}
+                userId={userId as string}
+                replyId={null}
+                isBookmarked={post.isBookmarked}
+              />
+              <button
+                className="rounded-full bg-transparent hover:bg-blue-400/10 hover:text-blue-400 p-2 my-1 text-white/50 cursor-pointer"
+                onClick={handleClick}
+              >
+                {" "}
+                <FiShare />{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {activeImage && (
+        <div
+          className={`fixed inset-0 z-50 flex justify-center bg-black/70 transition-all duration-300
+        ${showModal ? "opacity-100 scale-100" : "opacity-0 scale-95"}`}
+          onClick={(e) => {
+            e.stopPropagation();
+            setShowModal(false);
+            setTimeout(() => {
+              setActiveImage(null);
+            }, 300);
+          }}
+        >
+          <div className="absolute top-10 right-10"> <XIcon/> </div>
+            <Image
+              alt="image"
+              src={activeImage}
+              width={800}
+              height={800}
+              className="object-contain"
+              onClick={(e) => e.stopPropagation()}
+            />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
